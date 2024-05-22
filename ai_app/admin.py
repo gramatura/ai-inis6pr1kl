1842950ac575605168ce93f319d85408dfbea3dc
gramatura <129824984+gramatura@users.noreply.github.com>
2024-05-22 15:56:01 +0200

    osiem

---
+++ ai_app/admin.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from .models import ImageElement
+from .models import TextElement
+from .models import Book
+from .models import Music
+
+admin.site.register(ImageElement)
+admin.site.register(TextElement)
+admin.site.register(Book)
+admin.site.register(Music)
+
+
---
+++ ai_app/models/__init__.py
@@ -0,0 +1,5 @@
+# models/__init__.py
+from .ImageElement import ImageElement
+from .TitleElement import TextElement
+from .BookModel import Book
+from .Music import Music
